b12ce465a99292bafdbe67f610305e2b6fa87f15
ReolSt <roxman98@naver.com>
2020-12-23 23:57:50 +0900

    .

--- sort-visualization/main.js
+++ sort-visualization/main.js
@@ -2,9 +2,9 @@ let visualizerCanvas = document.getElementById("canvas-visualizer");
 let graphACanvas = document.getElementById("canvas-graph-a");
 let graphBCanvas = document.getElementById("canvas-graph-b");
 
-let visualizerContext = visualizerCanvas.getContext("webgl");
-let graphAContext = graphACanvas.getContext("webgl");
-let graphBContext = graphBCanvas.getContext("webgl");
+let visualizer = visualizerCanvas.getContext("webgl");
+let graphA = graphACanvas.getContext("webgl");
+let graphB = graphBCanvas.getContext("webgl");
 
 visualizerCanvas.width = visualizerCanvas.clientWidth;
 visualizerCanvas.height = visualizerCanvas.clientHeight;
@@ -21,3 +21,71 @@ let randomArray = getRandomArray_Integer(1, 1000, 1000);
 
 let timeStep = 5; /* milliseconds */
 let heightOffset = 50; /* px */
+
+let vertexShader = document.getElementById("vertex-shader");
+vertexShader = getShader(
+  visualizer,
+  visualizer.VERTEX_SHADER,
+  vertexShader.innerText
+);
+
+let fragmentShader = document.getElementById("fragment-shader");
+fragmentShader = getShader(
+  visualizer,
+  visualizer.FRAGMENT_SHADER,
+  fragmentShader.innerText
+);
+
+let mainProgram = getProgram(
+  visualizer,
+  [vertexShader, fragmentShader]
+);
+
+let positionAttributeLocation = visualizer.getAttribLocation(mainProgram, "a_position");
+
+let positionBuffer = visualizer.createBuffer();
+visualizer.bindBuffer(visualizer.ARRAY_BUFFER, positionBuffer);
+
+let positions = [
+  0, 0,
+  0, 0.5,
+  0.7, 0
+];
+
+visualizer.bufferData(
+  visualizer.ARRAY_BUFFER,
+  new Float32Array(positions),
+  visualizer.STATIC_DRAW
+);
+
+let vSize = 2;
+let vType = visualizer.FLOAT;
+let vNormalize = false;
+let vStride = 0;
+let vOffset = 0;
+
+function render() {
+  visualizer.viewport(0, 0, visualizer.canvas.width, visualizer.canvas.height);
+
+  visualizer.clearColor(0, 0, 0, 0);
+  visualizer.clear(visualizer.COLOR_BUFFER_BIT);
+  visualizer.useProgram(mainProgram);
+
+  
+  visualizer.VertexAttribPointer(
+    positionAttributeLocation,
+    vSize,
+    vType,
+    vNormalize,
+    vStride,
+    vOffset
+  );
+
+  let offset = 0;
+  let count = 3;
+  visualizer.drawArrays(
+    visualizer.TRIANGLES,
+    offset,
+    count
+  );
+}
---
+++ sort-visualization/shader.js
@@ -0,0 +1,33 @@
+a = 1;
+function getShader(glContext, type, source) {
+  a = source;
+  let shader = glContext.createShader(type);
+  glContext.shaderSource(shader, source);
+  glContext.compileShader(shader);
+
+  let success = glContext.getShaderParameter(shader, glContext.COMPILE_STATUS);
+  if(success) {
+    return shader;
+  }
+
+  console.error("Shader Compiling Error\n" + glContext.getShaderInfoLog(shader));
+  glContext.deleteShader(shader);
+}
+
+function getProgram(glContext, shaders) {
+  let program = glContext.createProgram();
+
+  shaders.forEach(shader => {
+    glContext.attachShader(program, shader);
+  });
+
+  glContext.linkProgram(program);
+
+  let success = glContext.getProgramParameter(program, glContext.LINK_STATUS);
+  if(success) {
+    return program;
+  }
+
+  console.error("Program Linking Error\n" + glContext.getProgramInfoLog(program));
+  glContext.deleteProgram(program);
+}
