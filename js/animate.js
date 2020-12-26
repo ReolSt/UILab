71263799baa98aea58ba96b0e132a0a7bd7c8267
ReolSt <roxman98@naver.com>
2020-12-26 20:50:47 +0900

    .

--- js/animate.js
+++ js/animate.js
@@ -1,20 +1,27 @@
 function addAnimation(htmlElement, animation) {
   let existingAnimation = getComputedStyle(htmlElement).animation;
-  htmlElement.style.animation = existingAnimation + ", " + animation;
+  if(existingAnimation.length === 0) {
+    htmlElement.style.animation = animation;
+  }
+  else {
+    htmlElement.style.animation = existingAnimation + ", " + animation;
+  }
 }
 
 let welcomeNextButton = document.getElementById("welcome-next");
 welcomeNextButton.addEventListener("click", event => {
   let welcomeContainer = document.getElementById("welcome");
-  welcomeContainer.style.setProperty("transition", "1s ease");
-  welcomeContainer.style.setProperty("transform", "translateX(-100vw)");
+  let welcomeTitle = document.getElementById("welcome-title");
+  welcomeTitle.style.transition = "transform 1s ease";
+  welcomeTitle.style.transform = "translateX(-100vw)";
   addAnimation(welcomeNextButton, "fade-out 1s ease 0s both");
-  setTimeout(() => {
-    welcomeNextButton.parentElement.removeChild(welcomeNextButton);
-    let letsGoButton = document.createElement("div");
-    letsGoButton.className = "action-button letsgo transparent hidden";
-    letsGoButton.innerText = "let's go";
-    addAnimation(letsGoButton, "fade-in 1s ease 0s both");
-    document.getElementById("main").appendChild(letsGoButton);
+  let interval = setInterval(() => {
+    document.getElementById("contents-container").removeChild(welcomeContainer);
+
+    let introduceContainer = document.getElementById("introduce");
+    introduceContainer.classList.remove("hidden");
+    introduceContainer.style.opacity = "1";
+    
+    clearInterval(interval);
   }, 1000);
 });
