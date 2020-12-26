89866144b4cba3bbbe4c0d0c22cfa306257bdaa6
ReolSt <roxman98@naver.com>
2020-12-27 01:57:43 +0900

    .

--- js/animate.js
+++ js/animate.js
@@ -1,9 +1,8 @@
 function addAnimation(htmlElement, animation) {
   let existingAnimation = getComputedStyle(htmlElement).animation;
-  if(existingAnimation.length === 0) {
+  if (existingAnimation.length === 0) {
     htmlElement.style.animation = animation;
-  }
-  else {
+  } else {
     htmlElement.style.animation = existingAnimation + ", " + animation;
   }
 }
@@ -15,13 +14,11 @@ welcomeNextButton.addEventListener("click", event => {
   welcomeTitle.style.transition = "transform 1s ease";
   welcomeTitle.style.transform = "translateX(-100vw)";
   addAnimation(welcomeNextButton, "fade-out 1s ease 0s both");
-  let interval = setInterval(() => {
-    document.getElementById("contents-container").removeChild(welcomeContainer);
+  setTimeout(() => {
+    document.getElementById("contents").removeChild(welcomeContainer);
 
     let introduceContainer = document.getElementById("introduce");
     introduceContainer.classList.remove("hidden");
     introduceContainer.style.opacity = "1";
-    
-    clearInterval(interval);
   }, 1000);
 });
