2fb82c7c71d3ad359bdc44889447835755ccc2cb
ReolSt <roxman98@naver.com>
2021-01-04 19:58:57 +0900

    Add three.js files.
    Originallly I had learned WebGL but now I decided to switch to high-level API.

--- sort-visualization/js/main.js
+++ sort-visualization/js/main.js
@@ -1,91 +1,68 @@
-document.oncontextmenu = () => {return false;}
+import * as THREE from "../three/three.module.js"
+import { FirstPersonControls } from "../three/jsm/controls/FirstPersonControls.js"
+
+document.oncontextmenu = () => { return false; }
 
 let visualizerCanvas = document.getElementById("canvas-visualizer");
 
-let visualizer = visualizerCanvas.getContext("webgl2", {preserveDrawingBuffer: true});
+let visualizer = visualizerCanvas.getContext("webgl2");
 
 visualizerCanvas.width = visualizerCanvas.clientWidth;
 visualizerCanvas.height = visualizerCanvas.clientHeight;
 
-let objectRenderer = new GLObjectRenderer({
-  glContext: visualizer,
-  vertexShaderSource: document.getElementById("vertex-shader").innerText,
-  fragmentShaderSource: document.getElementById("fragment-shader").innerText
+let renderer = new THREE.WebGLRenderer({
+  canvas: visualizerCanvas,
+  antialias: true
+});
+renderer.setSize(visualizer.canvas.width, visualizer.canvas.height);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+let playerCamera = new THREE.PerspectiveCamera(...Object.values({
+  fov: 90,
+  aspect: renderer.domElement.width / renderer.domElement.height,
+  near: 0.1,
+  far: 10000
+}));
+playerCamera.position.set(0, 0, 1);
+
+window.addEventListener("resize", event => {
+  playerCamera.aspect = renderer.domElement.width / renderer.domElement.height;
 });
 
-objectRenderer.addObject("background-0", new GLRectangle(
-  [
-    [0.2, 0.2, 0],
-    [0.2, 0, 0],
-    [0, 0.2, 0],
-    [0, 0, 0]
-  ],
-  [255, 255, 255]
-));
+var camControls = new FirstPersonControls(playerCamera);
+camControls.lookSpeed = 0.4;
+camControls.movementSpeed = 20;
+camControls.noFly = true;
+camControls.lookVertical = true;
+camControls.constrainVertical = true;
+camControls.verticalMin = 1.0;
+camControls.verticalMax = 2.0;
+camControls.lon = -150;
+camControls.lat = 120;
 
-objectRenderer.addObject("background-1", new GLRectangle(
-  [
-    [0.2, 0.2, 0.2],
-    [0.2, 0, 0.2],
-    [0, 0.2, 0.2],
-    [0, 0, 0.2]
-  ],
-  [255, 0, 255]
-));
+let mainScene = new THREE.Scene();
 
-objectRenderer.addObject("background-2", new GLRectangle(
-  [
-    [0, 0.2, 0.2],
-    [0, 0.2, 0],
-    [0, 0, 0.2],
-    [0, 0, 0]
-  ],
-  [255, 0, 100]
-));
+let plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 2),
+  new THREE.MeshLambertMaterial({ color: "#26c728" })
+);
+plane.position.set(0, 0, -3);
+mainScene.add(plane);
 
-objectRenderer.addObject("background-3", new GLRectangle(
-  [
-    [0.2, 0, 0],
-    [0.2, 0.2, 0],
-    [0.2, 0, 0.2],
-    [0.2, 0.2, 0.2]
-  ],
-  [0, 0, 255]
-));
+let cube = new THREE.Mesh(
+  new THREE.BoxGeometry(0.5, 0.5, 0.5),
+  new THREE.MeshLambertMaterial({ color: "#2e97f2" })
+);
+cube.position.set(0, 0, -2);
+mainScene.add(cube);
 
-objectRenderer.addObject("background-3", new GLRectangle(
-  [
-    [0.2, 0.2, 0.2],
-    [0.2, 0.2, 0],
-    [0, 0.2, 0.2],
-    [0, 0.2, 0]
-  ],
-  [0, 0, 255]
-));
+let light = new THREE.DirectionalLight("#FFFFFF", 2);
+light.castShadow = true;
+light.position.set(1, 4, 2);
+mainScene.add(light);
 
-function updateRotateMatrix() {
-  let sinX = Math.sin(rotateX), cosX = Math.cos(rotateX);
-  let sinY = Math.sin(rotateY), cosY = Math.cos(rotateY);
-  let sinZ = Math.sin(rotateZ), cosZ = Math.cos(rotateZ);
-  rotateMatrix[0][0] = cosX * cosY;
-  rotateMatrix[0][1] = cosX * sinY * sinZ - sinX * cosZ;
-  rotateMatrix[0][2] = cosX * sinY * cosZ + cosX * sinZ;
-  rotateMatrix[1][0] = sinX * cosY;
-  rotateMatrix[1][1] = sinX * sinY * sinZ + cosX * cosZ;
-  rotateMatrix[1][2] = sinX * sinY * cosZ - cosX * sinZ;
-  rotateMatrix[2][0] = sinY;
-  rotateMatrix[2][1] = cosY * sinZ;
-  rotateMatrix[2][2] = cosY * cosZ;
-}
 
-let cameraPosition = [0, 0, 0];
-
-let rotateMatrix = [
-  [1, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 1, 0],
-  [0, 0, 0, 1]
-];
 
 let keys = {};
 
@@ -97,84 +74,49 @@ document.body.addEventListener("keyup", event => {
   keys[event.key] = false;
 });
 
-let sensitivity = 0.002;
+let mouseSensitivity = 0.005;
 
-let mouseButtons = {};
 
-document.body.addEventListener("mousedown", event => {
-  mouseButtons[event.button] = true;
-})
+document.body.addEventListener("mousemove", event => {});
 
-document.body.addEventListener("mousemove", event => {
-  if(mouseButtons[0] === true) {
-    cameraPan += sensitivity * event.movementX;
-    cameraTilt += sensitivity * event.movementY;
-    updateCameraRotateMatrix();
-  }
+let moveSpeed = 0.01;
 
-  if(mouseButtons[1] === true) {
-    rotateY += sensitivity * event.movementX;
-    updateRotateMatrix();
-  }
+let jumpAnimation = false;
+let jumpAnimationKey = 0;
 
-  if(mouseButtons[2] === true) {
-    rotateX += sensitivity * event.movementY;
-    rotateY += sensitivity * event.movementX;
-    updateRotateMatrix();
-  }
-
-  rotateX += sensitivity * event.movementY;
-  rotateY += sensitivity * event.movementX;
-  updateRotateMatrix();
-});
-
-document.body.addEventListener("mouseup", event => {
-  mouseButtons[event.button] = false;
-});
-
-document.body.addEventListener("wheel", event => {
-  if(event.deltaY > 0) {
-    cameraPosition[1] -= 0.05;
-  }
-  else if(event.deltaY < 0) {
-    cameraPosition[1] += 0.05;
-  }
-});
-
-let rotateX = 0;
-let rotateY = 0;
-let rotateZ = 0;
-
-function handleKeyboardEvent() {
-  if(keys["w"]) {
-    cameraPosition[2] += 0.01;
-  }
-  if(keys["a"]) {
-    cameraPosition[0] -= 0.01;
-  }
-  if(keys["s"]) {
-    cameraPosition[2] -= 0.01;
-  }
-  if(keys["d"]) {
-    cameraPosition[0] += 0.01;
+function jump() {
+  playerCamera.position.y += (20 - jumpAnimationKey * 2) / 500;
+  jumpAnimationKey += 1;
+  if (jumpAnimationKey > 20) {
+    jumpAnimation = false;
+    jumpAnimationKey = 0;
   }
 }
 
-let renderloop = setInterval(() => {
-  handleKeyboardEvent();
+function render() {
+  if (keys["w"]) {
+    playerCamera.position.z -= moveSpeed;
+  }
+  if (keys["s"]) {
+    playerCamera.position.z += moveSpeed;
+  }
+  if (keys["a"]) {
+    playerCamera.position.x -= moveSpeed;
+  }
+  if (keys["d"]) {
+    playerCamera.position.x += moveSpeed;
+  }
 
-  objectRenderer.render((program, buffers) => {
-    visualizer.uniform3f(
-      program.uniformLocation("cameraPosition"),
-      cameraPosition[0],
-      cameraPosition[1],
-      cameraPosition[2]
-    )
+  if (keys[" "] && !jumpAnimation) {
+    jumpAnimation = true;
+  }
 
-    visualizer.uniformMatrix4fv(
-      program.uniformLocation("cameraRotationMatrix"),
-      false,
-      new Float32Array(getFlattenArray(rotateMatrix))
-    );
-  });
-}, 33);
+  if (jumpAnimation) {
+    jump();
+  }
+
+  renderer.render(mainScene, playerCamera);
+  requestAnimationFrame(render);
+}
+
+render();
