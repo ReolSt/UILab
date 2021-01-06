a4831fcf8bc25ff8036e577f8d0d36a091db9ea5
ReolSt <roxman98@naver.com>
2021-01-06 23:59:26 +0900

    start to use dat.GUI

--- sort-visualizer/js/main.js
+++ sort-visualizer/js/main.js
@@ -1,6 +1,17 @@
 import * as THREE from "../three/build/three.module.js";
 import Stats from '../three/examples/jsm/libs/stats.module.js';
-import GUI from '../three/examples/jsm/libs/dat.gui.module.js';
+import { GUI } from '../three/examples/jsm/libs/dat.gui.module.js';
+
+function createEquirectangularTexture(imgSrc, callback) {
+  let texture = new THREE.TextureLoader().load(
+    "resources/images/background-equirectangular.png",
+    () => {
+      let renderTarget = new THREE.WebGLCubeRenderTarget(texture.image.height);
+      renderTarget.fromEquirectangularTexture(renderer, texture);
+      callback(renderTarget);
+    }
+  );
+}
 
 document.oncontextmenu = () => { return false; }
 
@@ -13,6 +24,12 @@ visualizerCanvas.height = visualizerCanvas.clientHeight;
 document.body.requestPointerLock = document.body.requestPointerLock ||
                                    document.body.element.mozRequestPointerLock ||
                                    document.body.webkitRequestPointerLock;
+
+let isPointerLocked = false;
+
+document.addEventListener("pointerlockchange", event => {
+  isPointerLocked = !isPointerLocked;
+});
 
 let renderer = new THREE.WebGLRenderer({
   canvas: visualizerCanvas,
@@ -29,6 +46,7 @@ let playerCamera = new THREE.PerspectiveCamera(...Object.values({
   near: 0.1,
   far: 10000
 }));
+
 playerCamera.rotation.order = "YXZ";
 playerCamera.position.set(0, 0, 1);
 
@@ -38,49 +56,40 @@ window.addEventListener("resize", event => {
 
 let mainScene = new THREE.Scene();
 
-let backgroundTextureLoader = new THREE.TextureLoader();
-let backgroundTexture = new THREE.TextureLoader().load(
-  "resources/images/background-equirectangular.png",
-  () => {
-    let renderTarget = new THREE.WebGLCubeRenderTarget(backgroundTexture.image.height);
-    renderTarget.fromEquirectangularTexture(renderer, backgroundTexture);
-    mainScene.background = renderTarget;
-  }
+createEquirectangularTexture("resources/images/background-equirectangular.png", renderTarget => {
+  mainScene.background = renderTarget;
+})
+
+let light = new THREE.DirectionalLight("#ffffbb", 1);
+light.castShadow = true;
+light.position.set(1, 4, 2);
+mainScene.add(light);
+
+let plane = new THREE.Mesh(
+  new THREE.BoxGeometry(20, 20, 2),
+  new THREE.MeshToonMaterial({ color: "#26c728" })
 );
+plane.position.set(0, -2, 0);
+plane.rotateX(THREE.MathUtils.degToRad(90));
 
-{
-  let light = new THREE.DirectionalLight("#ffffbb", 1);
-  light.castShadow = true;
-  light.position.set(1, 4, 2);
-  mainScene.add(light);
-}
+mainScene.add(plane);
 
-{
-  let plane = new THREE.Mesh(
-    new THREE.BoxGeometry(20, 20, 2),
-    new THREE.MeshToonMaterial({ color: "#26c728" })
-  );
-  plane.position.set(0, -2, 0);
-  plane.rotateX(THREE.MathUtils.degToRad(90));
-
-  mainScene.add(plane);
-}
-
-{
-  let cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshToonMaterial({ color: "#2e97f2" })
-  );
-  cube.frustrumCulled = false;
-  cube.renderOrder = 1;
-  cube.position.set(0, 0, -2);
-  mainScene.add(cube);
-}
+let sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 32, 32),
+  new THREE.MeshToonMaterial({ color: "#2e97f2" })
+);
+sphere.frustrumCulled = false;
+sphere.position.set(0, 0, -2);
+mainScene.add(sphere);
 
 let keys = {};
 
 document.body.addEventListener("keydown", event => {
   keys[event.key] = true;
+  
+  if(event.key === "Control" && !isPointerLocked) {
+    document.body.requestPointerLock();
+  }
 });
 
 document.body.addEventListener("keyup", event => {
@@ -89,13 +98,11 @@ document.body.addEventListener("keyup", event => {
 
 let mouseSensitivity = 0.005;
 
-document.body.addEventListener("click", event => {
-  document.body.requestPointerLock();
-});
-
 document.body.addEventListener("mousemove", event => {
-  playerCamera.rotation.x = THREE.MathUtils.clamp(-Math.PI / 4, playerCamera.rotation.x - mouseSensitivity * event.movementY, Math.PI / 3);
-  playerCamera.rotation.y -= mouseSensitivity * event.movementX;
+  if(isPointerLocked) {
+    playerCamera.rotation.x = THREE.MathUtils.clamp(-Math.PI / 4, playerCamera.rotation.x - mouseSensitivity * event.movementY, Math.PI / 3);
+    playerCamera.rotation.y -= mouseSensitivity * event.movementX;
+  }
 });
 
 let moveSpeed = 0.05;
@@ -112,19 +119,43 @@ function jump() {
   }
 }
 
-
 let stats = new Stats();
 document.body.appendChild(stats.dom);
 
-/*
-
 let panel = new GUI({width: 310});
 
-let folderCamera = panel.addFolder("camera");
+let cameraControls = new function() {
+  this.fov = playerCamera.fov;
+  this.near = playerCamera.near;
+  this.far = playerCamera.far;
+};
 
-*/
+let folderCamera = panel.addFolder("camera");
+folderCamera.add(cameraControls, "fov", 60, 120);
+folderCamera.add(cameraControls, "near", 0.01, 1);
+folderCamera.add(cameraControls, "far", 10, 10000);
+folderCamera.open();
+
+function updateCameraValues() {
+  playerCamera.fov = cameraControls.fov;
+  playerCamera.near = cameraControls.near;
+  playerCamera.far = cameraControls.far;
+  playerCamera.updateProjectionMatrix();
+}
+
+let lightControls = new function() {
+  this.x = light.x;
+  this.y = light.y;
+  this.z = light.z; 
+}
+
+let sphereControls = new function() {
+  this.material = sphere.material.type;
+  this.radius = sphere.radius;
+}
 
 function render() {
+  updateCameraValues();
   if (keys["w"]) {
     playerCamera.position.z -= moveSpeed * Math.cos(playerCamera.rotation.y);
     playerCamera.position.x -= moveSpeed * Math.sin(playerCamera.rotation.y);
